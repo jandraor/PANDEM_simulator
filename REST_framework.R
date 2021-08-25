e4420581c08c09b302a679892c588218d52bb7e2
Jair Andrade <jair.albert.andrade@gmail.com>
2021-08-25 18:12:40 +0100

    Add framework for model 02

--- REST_framework.R
+++ REST_framework.R
@@ -13,16 +13,15 @@ function(req) {
   }
   
   fldr_path <- create_sim_folder(model_id)
+  par_list  <- extract_pars(user_args, model_id)
   
-  par_list <- extract_pars(user_args, model_id)
-  
-  ci <- ifelse(is.null(user_args$ci), 0, user_args$ci)
+  ci   <- ifelse(is.null(user_args$ci), 0, user_args$ci)
   sens <- ifelse(is.null(user_args$sens), 0, user_args$sens)
 
 
   if(sens == 0) {
     
-    sim_results <- run_model(par_list, fldr_path) 
+    sim_results <- run_model(model_id, par_list, fldr_path) 
     
     if(ci == 0) {
       return(jsonlite::toJSON(sim_results))
@@ -128,7 +127,7 @@ create_sim_folder <- function(model_id) {
   
 }
 
-run_model <- function(par_list, fldr_path) {
+run_model <- function(model_id, par_list, fldr_path) {
   
   input_file <- file.path(fldr_path, "inputs.txt")
   if(file.exists(input_file)) file.remove(input_file)
@@ -140,13 +139,11 @@ run_model <- function(par_list, fldr_path) {
   inputs     <- as.data.frame(par_list)
   readr::write_tsv(inputs, input_file)
   
-  mdl_path <- file.path(fldr_path, "model_01.stmx")
+  mdl_path <- file.path(fldr_path, paste0(model_id, ".stmx"))
   sys_cmd  <- paste("./stella_simulator", mdl_path)
   system(sys_cmd)
   
-  readr::read_tsv(output_file) |>
-    dplyr::mutate(dC = C - dplyr::lag(C)) |>
-    dplyr::filter(Day != 0)
+  readr::read_tsv(output_file)
 }
 
 extract_pars <- function(user_args, model_id) {
@@ -157,12 +154,106 @@ extract_pars <- function(user_args, model_id) {
                      par_gamma = user_args$gamma,
                      par_sigma = user_args$sigma,
                      gamma     = user_args$gamma,
-                     S         = user_args$S0,
-                     E         = user_args$E0,
-                     I         = user_args$I0,
-                     R         = user_args$R0)
+                     S         = user_args$S,
+                     E         = user_args$E,
+                     I         = user_args$I,
+                     R         = user_args$R)
+  }
+  
+  if(model_id == "model_02") {
+    
+    pop_size   <- 1e4
+    
+    whp        <- c("S", "E", "I", "R") # Within-host profile
+    groups     <- c("A", "B", "C", "D")
+    api_stocks <- paste(rep(whp, 4),  rep(groups, each = 4), sep = "_")
+    
+    expected_pars <- c(api_stocks, "q", "gamma", "sigma", "cm")
+    actual_pars   <- names(user_args)
+    vald          <- expected_pars %in% actual_pars # Validation
+    
+    if(any(vald == FALSE) {
+      mis_par <- paste(expected_pars[vald], collapse = ", ") # Missing params
+      msg     <- stringr::str_glue("Parameters '{mis_par}' not found")
+      stop(msg, call. = FALSE)
+    })
+      
+    valid_cms <- c("Belgium", "Germany", "Finland", "United Kingdom",
+                   "Italy", "Luxembourg", "Netherlands", "Poland")
+    
+    if(!cm %in% valid_cms) {
+      msg <- stringr::str_glue("Contact matrix for '{cm}' not available")
+      stop(msg, call. = FALSE)
+    }
+      
+    age_limits <- c(0, 5, 15, 45)
+    
+    cm_object <- contact_matrix(polymod, age.limits = age_limits,
+                                countries  = cm,
+                                survey.pop = cm,
+                                symmetric  = TRUE)
+    
+    pop_df    <- cm_object$demography |> 
+      dplyr::rename(age_group  = age.group) |>
+      dplyr::mutate(age_group  = format_age_group(as.character(age_group)),
+                    group_name = groups,
+                    population = discrete_separation(proportion, pop_size))
+    
+    w_matrix           <- cm_object$matrix
+    colnames(w_matrix) <- groups
+    rownames(w_matrix) <- groups
+    
+    contact_list        <- c(w_matrix) |> as.list()
+    combs               <- paste0(rep(groups, each = 4), rep(groups, 4))
+    names(contact_list) <- stringr::str_glue("w[{combs}]")
+
+    stock_list <- user_args[api_stocks]
+    
+    stocks_df <- data.frame(id         = api_stocks, 
+                            pct        = as.numeric(stock_list),
+                            group_name = rep(groups, each = 4)) |> 
+      dplyr::left_join(pop_df[ ,c("group_name", "population")]) |>
+      dplyr::group_by(group_name) |>
+      dplyr::mutate(pop_by_age = discrete_separation(pct, unique(population)))
+    
+    stl_stock_names   <- stringr::str_glue("{rep(whp, each = 4)}[{rep(groups, 4)}]")
+    stock_list        <- as.list(stocks_df$pop_by_age)
+    names(stock_list) <- as.character(stl_stock_names)
+    
+    par_list <- c(stock_list, contact_list)
+    
+    par_list$par_gamma <- user_args$gamma
+    par_list$par_sigma <- user_args$sigma
+    par_list$q         <- user_args$q
   }
   
   par_list
 }
 
+format_age_group <- function(ag_vector) {
+  pattern <- stringr::regex("\\[(\\d+),(\\d+)\\)")
+  
+  new_ag_vector <- vector(length = length(ag_vector), mode = "character") 
+  
+  for(i in seq_along(ag_vector)) {
+    current_ag <- ag_vector[[i]]
+    
+    if(stringr::str_detect(current_ag, pattern)) {
+      output_sm   <- stringr::str_match(current_ag, pattern)
+      lower_bound <- output_sm[[2]] %>% stringr::str_pad(width = 2, pad = "0")
+      upper_bound <- (as.numeric(output_sm[[3]]) - 1) %>% 
+        stringr::str_pad(width = 2, pad = "0")
+      current_ag  <- paste(lower_bound, upper_bound, sep = "-")
+    }
+    
+    new_ag_vector[[i]] <- current_ag
+  }
+  new_ag_vector
+}
+
+discrete_separation <- function(props, total) {
+  vals2 <- round(total * props[-1], 0)
+  vals1 <- total - sum(vals2)
+  
+  c(vals1, vals2)
+}
