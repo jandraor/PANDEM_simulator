61e11e9125483f0af1ac93c62e61a0509308664e
Jair Andrade <jair.albert.andrade@gmail.com>
2021-09-06 14:51:14 +0100

    Fix path

--- run_api.R
+++ run_api.R
@@ -3,4 +3,4 @@
 library(plumber)
 files <- file.path("./R",list.files("./R"))
 sapply(files, source)
-pr("REST_framework.R") |> pr_run(host="0.0.0.0", port = 8000)
+pr("./R/REST_framework.R") |> pr_run(host="0.0.0.0", port = 8000)
