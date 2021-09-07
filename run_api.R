bdbbc743a1b36ece55e5bccf649a3caca32cb668
Jair Andrade <jair.albert.andrade@gmail.com>
2021-09-07 18:49:23 +0100

    Update API script

--- run_api.R
+++ run_api.R
@@ -1,6 +1,6 @@
 #!/usr/bin/env Rscript
 
 library(plumber)
-files <- file.path("./R",list.files("./R"))
-sapply(files, source)
-pr("./R/REST_framework.R") |> pr_run(host="0.0.0.0", port = 8000)
+setwd("./R")
+source("./R/create_sim_folder.R")
+pr("./R/REST_framework.R") |> pr_run(host = "0.0.0.0", port = 8000)
