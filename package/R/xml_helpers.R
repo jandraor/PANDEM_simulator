63512dc17efe0103931ae5ac56b9678d041b2b22
Jair Andrade <jair.albert.andrade@gmail.com>
2022-06-16 17:59:55 +0100

    Add default values to params API

--- package/R/xml_helpers.R
+++ package/R/xml_helpers.R
@@ -36,29 +36,83 @@ create_par_obj <- function(module_xml, dims_obj) {
   vars_xml   <- xml2::xml_find_first(module_xml, ".//d1:variables")
   auxs_xml   <- xml2::xml_find_all(vars_xml, ".//d1:aux")
 
-  lapply(auxs_xml, \(aux_obj) {
+  lapply(auxs_xml, format_par_obj, module_name, dims_obj)
+}
 
-    aux_name <- xml2::xml_attr(aux_obj, "name")
+format_par_obj <- function(aux_obj, category, dims_obj) {
 
-    dims_aux <- xml2::xml_find_first(aux_obj, ".//d1:dimensions")
+  aux_name <- xml2::xml_attr(aux_obj, "name")
 
-    is_arrayed <- ifelse(length(dims_aux) > 0, TRUE, FALSE)
+  dims_aux <- xml2::xml_find_first(aux_obj, ".//d1:dimensions")
 
-    par_obj <- list(name     = aux_name,
-                    category = category,
-                    array    = is_arrayed)
+  is_arrayed <- ifelse(length(dims_aux) > 0, TRUE, FALSE)
 
-    if(is_arrayed) {
+  name <- sanitise_name(aux_name)
 
-      dim_tags  <- xml2::xml_find_all(dims_aux, ".//d1:dim")
-      dim_names <- sapply( dim_tags, \(elem_tag) xml2::xml_attr(elem_tag, "name"))
+  par_obj <- list(name     = name,
+                  category = category,
+                  array    = is_arrayed)
 
-      dims_list         <- lapply(dim_names, \(dim_name) dims_obj[[dim_name]])
-      names(dims_list)  <- dim_names
+  if(!is_arrayed) {
 
-      par_obj$dimensions <- dims_list
+    def_val <- xml2::xml_find_first(aux_obj, ".//d1:eqn") |>
+      xml2::xml_text() |> as.numeric()
+
+    dv_list         <- list(def_val)
+    names(dv_list)  <- stringr::str_glue("{category}.{aux_name}")
+
+    par_obj$default_values <- dv_list
+  }
+
+  if(is_arrayed) {
+
+    dim_tags  <- xml2::xml_find_all(dims_aux, ".//d1:dim")
+    dim_names <- sapply( dim_tags, \(elem_tag) xml2::xml_attr(elem_tag, "name"))
+
+    dims_list         <- lapply(dim_names, \(dim_name) dims_obj[[dim_name]])
+    names(dims_list)   <- dim_names
+
+    par_obj$dimensions <- dims_list
+
+    cld_xml      <- xml2::xml_children(aux_obj)
+    child_names  <- xml2::xml_name(cld_xml)
+
+    if("eqn" %in% child_names) {
+      par_val <- xml2::xml_find_first(aux_obj, ".//d1:eqn") |>
+        xml2::xml_text() |> as.numeric()
+
+      combs <- purrr::cross(dims_list)
+
+      elems <- purrr::map_chr(combs, \(elem_obj) paste(elem_obj, collapse = ","))
+
+      elems_list <- rep(par_val, length(elems)) |> as.list()
+
+
     }
 
-    par_obj
-  })
+    if(!("eqn" %in% child_names)) {
+
+      elements_xml <- xml2::xml_find_all(aux_obj, ".//d1:element")
+
+      elems <- xml2::xml_attr(elements_xml, "subscript") |>
+        stringr::str_remove(" ")
+
+      elems_list <- xml2::xml_find_all(elements_xml, ".//d1:eqn") |>
+        xml2::xml_text() |> as.numeric() |> as.list()
+
+    }
+
+    labels <- stringr::str_glue("{category}.{name}[{elems}]")
+
+    names(elems_list) <- labels
+
+    par_obj$default_values <- elems_list
+  }
+
+  par_obj
+}
+
+
+sanitise_name <- function(name) {
+  stringr::str_replace_all(name, " ", "_")
 }
---
+++ package/tests/testthat/test-xml_helpers.R
@@ -0,0 +1,140 @@
+
+test_var_xml <- xml2::read_xml('
+  <root>
+    <doc1 xmlns = "http://docs.oasis-open.org/xmile/ns/XMILE/v1.0">
+	<dimensions>
+		<dim name="Age">
+			<elem name="Young"/>
+			<elem name="Adult"/>
+		</dim>
+		<dim name="Age_contactee">
+			<elem name="Young"/>
+			<elem name="Adult"/>
+		</dim>
+		<dim name="Age_contactor">
+			<elem name="Young"/>
+			<elem name="Adult"/>
+		</dim>
+		<dim name="Region">
+			<elem name="NL13"/>
+			<elem name="NL21"/>
+			<elem name="NL22"/>
+		</dim>
+	</dimensions>
+	    <model name="Inputs_Epi">
+	      <variables>
+			    <aux name="ClinicalFraction" access="output">
+					  <dimensions>
+					    <dim name="Age"/>
+				    </dimensions>
+				    <eqn>.5</eqn>
+				  </aux>
+			    <aux name="Pathogen Seed Time" access="output">
+				    <dimensions>
+					    <dim name="Region"/>
+					    <dim name="Age"/>
+				    </dimensions>
+				    <eqn>0</eqn>
+			    </aux>
+			    <aux name="Infectivity" access="output">
+				    <eqn>0.1</eqn>
+			    </aux>
+			    <aux name="BaseContacts" access="output">
+			      <dimensions>
+			        <dim name="Age_contactor"/>
+					    <dim name="Age_contactee"/>
+				    </dimensions>
+				    <element subscript="Young, Young">
+				      <eqn>1</eqn>
+				    </element>
+				    <element subscript="Young, Adult">
+					    <eqn>2</eqn>
+				    </element>
+				    <element subscript="Adult, Young">
+					    <eqn>3</eqn>
+				    </element>
+				    <element subscript="Adult, Adult">
+					    <eqn>4</eqn>
+				    </element>
+			    </aux>
+		    </variables>
+		  </model>
+    </doc1>
+  </root>')
+
+test_that("format_par_obj returns the expected list for array with a common value", {
+
+  module_xml  <- xml2::xml_find_first(test_var_xml, ".//d1:model")
+  module_name <- xml2::xml_attr(module_xml, "name")
+
+  dims_obj    <- create_dims_obj(test_var_xml)
+
+  vars_xml   <- xml2::xml_find_first(module_xml, ".//d1:variables")
+  aux_obj    <- xml2::xml_find_first(vars_xml, ".//d1:aux")
+
+  actual   <- format_par_obj(aux_obj, module_name, dims_obj)
+
+  expected <- list(
+    name           = "ClinicalFraction",
+    category       = module_name,
+    array          = TRUE,
+    dimensions     = list(Age = c("Young", "Adult")),
+    default_values = list(`Inputs_Epi.ClinicalFraction[Young]` = 0.5,
+                          `Inputs_Epi.ClinicalFraction[Adult]` = 0.5))
+
+  expect_equal(actual, expected)
+})
+
+test_that("format_par_obj returns the expected list for array with a common value with two or more dimensions", {
+
+  module_xml  <- xml2::xml_find_first(test_var_xml, ".//d1:model")
+  module_name <- xml2::xml_attr(module_xml, "name")
+
+  dims_obj    <- create_dims_obj(test_var_xml)
+
+  vars_xml   <- xml2::xml_find_first(module_xml, ".//d1:variables")
+  aux_obj    <- xml2::xml_find_all(vars_xml, ".//d1:aux")[[2]]
+
+  actual   <- format_par_obj(aux_obj, module_name, dims_obj)
+
+  expected <- list(
+    name           = "Pathogen_Seed_Time",
+    category       = module_name,
+    array          = TRUE,
+    dimensions     = list(Region = c("NL13", "NL21", "NL22"),
+                          Age    = c("Young", "Adult")),
+    default_values = list(`Inputs_Epi.Pathogen_Seed_Time[NL13,Young]` = 0,
+                          `Inputs_Epi.Pathogen_Seed_Time[NL21,Young]` = 0,
+                          `Inputs_Epi.Pathogen_Seed_Time[NL22,Young]` = 0,
+                          `Inputs_Epi.Pathogen_Seed_Time[NL13,Adult]` = 0,
+                          `Inputs_Epi.Pathogen_Seed_Time[NL21,Adult]` = 0,
+                          `Inputs_Epi.Pathogen_Seed_Time[NL22,Adult]` = 0))
+
+  expect_equal(actual, expected)
+})
+
+test_that("format_par_obj returns the expected list for a multidimensional array with specific values", {
+
+  module_xml  <- xml2::xml_find_first(test_var_xml, ".//d1:model")
+  module_name <- xml2::xml_attr(module_xml, "name")
+
+  dims_obj    <- create_dims_obj(test_var_xml)
+
+  vars_xml   <- xml2::xml_find_first(module_xml, ".//d1:variables")
+  aux_obj    <- xml2::xml_find_all(vars_xml, ".//d1:aux")[[4]]
+
+  actual   <- format_par_obj(aux_obj, module_name, dims_obj)
+
+  expected <- list(
+    name           = "BaseContacts",
+    category       = module_name,
+    array          = TRUE,
+    dimensions     = list(Age_contactor = c("Young", "Adult"),
+                          Age_contactee = c("Young", "Adult")),
+    default_values = list(`Inputs_Epi.BaseContacts[Young,Young]` = 1,
+                          `Inputs_Epi.BaseContacts[Young,Adult]` = 2,
+                          `Inputs_Epi.BaseContacts[Adult,Young]` = 3,
+                          `Inputs_Epi.BaseContacts[Adult,Adult]` = 4))
+
+  expect_equal(actual, expected)
+})
