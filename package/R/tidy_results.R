e141aa837548ec8073cd5267f0d92b3cb7246f0a
Jair Andrade <jair.albert.andrade@gmail.com>
2022-08-08 14:50:44 +0100

    Fix bug in outputs

--- package/R/tidy_results.R
+++ package/R/tidy_results.R
@@ -14,9 +14,7 @@ tidy_results <- function(sim_results, model_id) {
 
     var_name <- indicator_obj$name
 
-    data_df <- dplyr::select(sim_results, Days, dplyr::contains(var_name)) |>
-      tidyr::pivot_longer(-Days) |>
-      dplyr::mutate(name = stringr::str_replace(name, "^.+\\.", ""))
+    data_df <- extract_indicator(sim_results, var_name, indicator_obj$array)
 
     if(indicator_obj$array) {
 
@@ -36,5 +34,17 @@ tidy_results <- function(sim_results, model_id) {
 
   result_list
 
+
+}
+
+extract_indicator <- function(sim_results, var_name, is_array) {
+
+  if(!is_array) regex <- stringr::str_glue("^.+\\.{var_name}$")
+
+  if(is_array) regex <- stringr::str_glue("^.+\\.{var_name}\\[.+\\]")
+
+  dplyr::select(sim_results, Days, dplyr::matches(regex)) |>
+    tidyr::pivot_longer(-Days) |>
+    dplyr::mutate(name = stringr::str_replace(name, "^.+\\.", ""))
 
 }
--- package/tests/testthat/test-tidy_results.R
+++ package/tests/testthat/test-tidy_results.R
@@ -9,3 +9,59 @@ test_that("tidy_results() returns objects with the expected names", {
 
   expect_equal(actual, expected)
 })
+
+test_that("extract_indicator() returns the expected data frame", {
+
+  sim_results <- data.frame(
+    Days = 0,
+    Hospital_Resources.ICU_Beds = 0,
+    Hospital_Resources.occupied_ICU_beds = 0,
+    Hospital.physical_ICU_beds_available = 0,
+    Hospital.expected_ICU_beds_freed = 0,
+    Hospital.physical_ICU_beds_needed = 0,
+    Hospital.physical_ICU_beds_gap = 0,
+    Hospital_Resources.staffed_equipped_ICU_beds_available = 0,
+    Hospital_Resources.staffed_equipped_ICU_beds_needed = 0,
+    Hospital_Resources.staffed_equipped_ICU_beds_gap = 0,
+    Hospital.peak_demand_ICU_beds = 0)
+
+  var_name <- "ICU_beds"
+
+  actual   <- extract_indicator(sim_results, var_name, FALSE)
+
+  expected <- data.frame(Days = 0, name = "ICU_Beds", value = 0)
+
+  expect_equal(as.data.frame(actual), expected)
+})
+
+test_that("extract_indicator() returns the expected data frame for an array", {
+
+  sim_results <- data.frame(0, 0, 0)
+
+  colnames(sim_results) <- c("Days", "Hospital.ICU admissions[y]",
+                             "Hospital.ICU admissions[o]")
+
+  var_name <- "ICU admissions"
+
+  actual   <- extract_indicator(sim_results, var_name, TRUE)
+
+  expected <- data.frame(Days = c(0, 0),
+                         name = c("ICU admissions[y]", "ICU admissions[o]"),
+                         value = c(0, 0))
+
+  expect_equal(as.data.frame(actual), expected)
+})
+
+test_that("extract_indicator() returns the expected data frame when the var name is also prefix", {
+
+  sim_results <- data.frame(0, 0, 0, 0)
+
+  colnames(sim_results) <- c("Days", "ctg.PPE", "ctg.PPE_needed", "ctg.PPE_gap")
+
+  var_name <- "PPE"
+  actual   <- extract_indicator(sim_results, var_name, FALSE)
+
+  expected <- data.frame(Days = 0, name = "PPE", value = 0)
+
+  expect_equal(as.data.frame(actual), expected)
+})
