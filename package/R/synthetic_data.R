805066d2f3ab77daf37774db422dbc4b7bf40093
Jair Andrade <jair.albert.andrade@gmail.com>
2022-12-06 17:09:01 +0000

    Add total confirmed cases indicator & update synthetic data generator script

--- package/R/synthetic_data.R
+++ package/R/synthetic_data.R
@@ -1,57 +1,48 @@
-generate_synthetic_data <- function(filepath) {
+generate_synthetic_data <- function(filepath, country) {
+
+
+  indicator_list <- list(
+    list(col_pattern   = "Total confirmed cases",
+         name          = "Cases",
+         concentration = 5),
+    list(col_pattern   = "Total Admissions",
+         name          = "Hospitalisations",
+         concentration = 15),
+    list(col_pattern   = "total deaths",
+         name          = "Deaths",
+         concentration = 15))
 
   set.seed(123)
 
-  df                 <- readr::read_tsv(filepath)
+  output_df <- readr::read_tsv(filepath)
 
-  cases_df           <- dplyr::select(df, Days, dplyr::contains("Total cases"))
+  purrr::map_df(indicator_list, \(indicator_obj) {
 
-  colnames(cases_df) <- c("Time", paste0("Cases_", LETTERS[1:3]))
+    col_pattern <- indicator_obj$col_pattern
 
+    indicator_df <- dplyr::select(output_df, Days,
+                              dplyr::contains(col_pattern))
 
-  tidy_cases <- cases_df |> tidyr::pivot_longer(-Time) |>
-    dplyr::group_by(name) |>
-    dplyr::mutate(Model_incidence = value - dplyr::lag(value, default = NA)) |>
-    dplyr::filter(!is.na(Model_incidence)) |>
-    dplyr::ungroup() |>
-    tidyr::separate(name, c("Var", "Age"),sep = "_") |>
-    dplyr::select(-value) |>
-    dplyr::group_by(Age) |>
-    dplyr::mutate(Synthetic_incidence = rnbinom(dplyr::n(),
-                                                mu = Model_incidence,
-                                                size = 10)) |>
-    dplyr::ungroup() |> dplyr::select(-Var)
+    colnames(indicator_df) <- c("Time", paste(indicator_obj$name,
+                                           LETTERS[1:3], sep = "_"))
 
+    conc <- indicator_obj$concentration
 
-  synthetic_cases <- tidyr::pivot_wider(tidy_cases,
-                                        values_from = c(Synthetic_incidence,
-                                                        Model_incidence),
-                                        names_from = Age)
+    tidy_df <- indicator_df|> tidyr::pivot_longer(-Time) |>
+      dplyr::group_by(name) |>
+      dplyr::mutate(model_val = value - dplyr::lag(value, default = NA)) |>
+      dplyr::filter(!is.na(model_val)) |>
+      dplyr::ungroup() |>
+      tidyr::separate(name, c("Var", "Age"),sep = "_") |>
+      dplyr::select(-value) |>
+      dplyr::group_by(Age) |>
+      dplyr::mutate(synthetic_val = rnbinom(dplyr::n(),
+                                                  mu = model_val,
+                                                  size = conc)) |>
+      dplyr::ungroup() |> dplyr::select(-Var) |>
+      dplyr::mutate(indicator = indicator_obj$name,
+             country   = country)
+  }) ->  synthetic_data
 
-  hosp_df <- dplyr::select(df, Days, dplyr::contains("Total Admissions"))
-
-  colnames(hosp_df) <- c("Time", paste0("Hospitalisations_", LETTERS[1:3]))
-
-  tidy_hosp <- hosp_df |> tidyr::pivot_longer(-Time) |>
-    dplyr::group_by(name) |>
-    dplyr::mutate(Model_hospitalisation = value - dplyr::lag(value, default = NA)) |>
-    dplyr::filter(!is.na(Model_hospitalisation)) |>
-    dplyr::ungroup() |>
-    tidyr::separate(name, c("Var", "Age"),sep = "_") |>
-    dplyr::select(-value) |>
-    dplyr::group_by(Age) |>
-    dplyr::mutate(Synthetic_hospitalisation = rnbinom(dplyr::n(),
-                                                mu = Model_hospitalisation,
-                                                size = Inf)) |>
-    dplyr::ungroup() |> dplyr::select(-Var)
-
-  synthetic_hospitalisation <- tidyr::pivot_wider(
-    tidy_hosp,
-    values_from = c(Synthetic_hospitalisation, Model_hospitalisation),
-    names_from = Age)
-
-  dplyr::bind_cols(synthetic_cases,
-                   dplyr::select(synthetic_hospitalisation, - Time))
-
-
+  readr::write_csv(synthetic_data, paste0("../Synthetic_data/", country,".csv"))
 }
