cc4f96f4d9d81d637ceb08f9a67aa33e42d251e9
Jair Andrade <jair.albert.andrade@gmail.com>
2023-05-08 14:02:54 +0100

    Fix prob inf API

--- package/R/get_upsilon.R
+++ package/R/get_upsilon.R
@@ -1,7 +1,7 @@
 get_upsilon <- function(R0, country_code, pop_a, pop_b, pop_c, pop_d) {
 
   upsilon_vals <- seq(0, 1, 0.1)
-  R0_vals      <-  purrr::map_dbl(seq(0, 1, 0.1), estimate_R0, 'DE',
+  R0_vals      <-  purrr::map_dbl(seq(0, 1, 0.1), estimate_R0, country_code,
                                   pop_a, pop_b, pop_c, pop_d)
 
   df <- data.frame(upsilon = upsilon_vals, R0 = R0_vals)
